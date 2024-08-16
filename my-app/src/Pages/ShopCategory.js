f7f3fa1129e117444516b7f20db97c5567f2bec9
Justin <justinmanoj12@gmail.com>
2024-08-15 21:58:58 -0400

    second set continued

---
+++ my-app/src/Pages/ShopCategory.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const ShopCategory = () => {
+    return (
+        <div>
+            <p> Shop Category </p>
+
+
+        </div>
+    )
+}
+
+
+export default ShopCategory; 
