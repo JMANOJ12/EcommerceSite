714d55dc1889230cd6ee8e7472c7c86cc1d7b469
Justin <justinmanoj12@gmail.com>
2024-08-17 12:29:36 -0400

    Changes to Files

---
+++ my-app/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,31 @@
+import React from 'react'; 
+import './DescriptionBox.css'; 
+
+
+
+const DescriptionBox = () => { 
+    return (
+        <div className= 'descriptionbox' >
+            <div className='descriptionbox-navigator'>
+                <div className='descriptionbox-nav-box'> Description </div>
+                <div className='descriptionbox-nav-box fade'> Reveiws (122) </div>
+
+            </div>
+
+            <div className='descriptionbox-description'>
+                <p>
+
+                    An e-commerce website is an online platform that facilitates buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.
+                    These platforms provide a wide range of features to enhance the shopping experience, such as product catalogs, search functionality, customer reviews, and secure payment gateways. They also offer various tools for businesses to manage inventory, track orders, and analyze sales data. With the rise of mobile commerce, many e-commerce websites are optimized for mobile devices, allowing customers to shop on the go.
+                </p>
+            
+            </div>
+
+        </div>
+        
+ 
+    )
+
+}
+
+export default DescriptionBox; 
--- my-app/src/Pages/Product.js
+++ my-app/src/Pages/Product.js
@@ -2,7 +2,10 @@ import React, { useContext } from 'react';
 import { ShopContext } from '../Context/ShopContext'; 
 import { useParams } from 'react-router-dom'; 
 import Breadcrum from '../Components/Breadcrums/Breadcrums';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'; 
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 
 const Product = () => {
@@ -13,8 +16,9 @@ const Product = () => {
     return (
         <div>
             <Breadcrum product={product} />
-
-
+            <ProductDisplay product={product} />
+            <DescriptionBox />
+            <RelatedProducts/>
         </div>
     )
 }
