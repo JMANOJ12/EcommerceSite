d02730ac84e4e149f3e7eb4123d46fa650f7eb8e
Justin <justinmanoj12@gmail.com>
2024-09-05 02:36:55 -0400

    Backend code and folder added

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const port = '4000'
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+const jwt = require('jsonwebtoken'); 
+const multer = require('multer'); 
+const path = require('path'); 
+const cors = require('cors'); 
+
+const app = express(); 
+
+
+app.use(express.json()); 
+app.use(cors()); 
+
+
+
+
+
+
