035edc63b12f35dc09473d023a5f222c5f5352a4
Justin <justinmanoj12@gmail.com>
2024-09-05 16:39:09 -0400

    changes to index.js file

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,50 @@ const app = express();
 app.use(express.json()); 
 app.use(cors()); 
 
+// Database Connection
+mongoose.connect('mongodb+srv://justin-dev:1234@cluster0.70tl9.mongodb.net/Ecommerce'); 
 
+// API Creation
+app.get('/', (req, res) => {
+    res.send('GET Request Called.')
+})
+
+
+
+
+app.listen(port, (err) => { 
+    if (!err) {
+        console.log("Server listening on PORT", port);
+    } else { 
+        console.lo("Error: " + err); 
+
+    }
+})
+
+
+// Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images', 
+    fileName: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`); 
+
+    }
+
+}); 
+
+const upload = multer({
+    storage: storage
+});
+
+
+// Creating Upload Endpoint For Images
+app.use('/images', express.static('upload/images')); 
+app.post('/upload', upload.single('product'), (req, res) => { 
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.fileName}`
+    })
+})
 
 
 
